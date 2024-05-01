399aa312dbeb3fdf16d42d11e400f801c3713a00
Rodrigo Prata <roserodrigo26@gmail.com>
2024-05-01 18:19:30 -0300

    testes negativos de hooks

--- cypress/e2e/3-loja-ebac/login.cy.js
+++ cypress/e2e/3-loja-ebac/login.cy.js
@@ -1,12 +1,40 @@
 ///<reference types="cypress"/>
 
 describe('funcionalidade: Login', () => {
-    it('deve fazer login com sucesso', () => {
+    beforeEach(() => {
         cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+    });
+
+    afterEach(() => {
+        cy.screenshot()
+    
+
+    });
+
+    it('deve fazer login com sucesso', () => {
+       
         cy.get('#username').type('rodrigo.prata@teste.com.br')
         cy.get('#password').type('teste@123')
         cy.get('.woocommerce-form > .button').click()
-
         cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain', 'Olá, rodrigo.prata (não é rodrigo.prata? Sair)')
+            
+    });
+
+    it('deve exibir uma mensagem de erro ao inserir usuario invalido', () => {
+
+        cy.get('#username').type('rodrigo@teste.com.br')
+        cy.get('#password').type('teste@123')
+        cy.get('.woocommerce-form > .button').click()
+        cy.get('.woocommerce-error').should('exist')
+
     })
+
+    it('deve exibir uma mensagem de erro ao inserir senha invalida', () => {
+        
+        cy.get('#username').type('rodrigo.prata@teste.com.br')
+        cy.get('#password').type('teste@000')
+        cy.get('.woocommerce-form > .button').click()
+        cy.get('.woocommerce-error').should('contain' , 'Erro: A senha fornecida para o e-mail rodrigo.prata@teste.com.br está incorreta. Perdeu a senha?')
+        cy.get('.woocommerce-error').should('exist')
+    });
 })
